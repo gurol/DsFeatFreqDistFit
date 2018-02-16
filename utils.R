a8684398896653cc8beec7a3b63a03ef55448264
Gürol Ca <gurol44@gmail.com>
2018-02-16 11:58:39 +0300

    utils - Common utility R functions

--- utils.R
+++ utils.R
@@ -4,15 +4,16 @@
 #' @references <http://gurol.canbek.com>  
 #' @keywords utilities, common functions  
 #' @title utils - Common utility R functions  
-#' @date 1 January 2017  
+#' @date 16 January 2017  
 #' @version 1.2  
 #' @note version history  
+#' 1.2, 16 Feburary 2018, Plot to devide or PNG file
 #' 1.1, 14 February 2018, Column name checking avoiding parameter for rclip
 #' 1.0, 1 February 2017, The first version  
 #' @description Common R functions that can be called from other scripts
 
 #' libraries  
-library(parallel)
+library(parallel) # Preinstalled in environment
 
 #' ### getNumberOfCPUCores
 #' Return the number of CPU cores in the current host  
@@ -125,4 +126,34 @@ renameDataFrameColumn<-function(df, column_name, new_column_name)
   colnames(df)[colnames(df)==column_name] <- new_column_name
   
   return(df)
+}
+
+#' ### plotToDeviceOrFile
+#' Plot a graphic to a device of a PNG file (if a file name is given)  
+#' **Parameters:**  
+#' *data*: data or R object to plot  
+#' *main_title*: n overall title for the plot (default: NULL)  
+#' *xlabel*: a title for the x axis (default: NULL)  
+#' *ylabel*: a title for the x axis (default: NULL)  
+#' *filepath*: path of the file (default: NULL to plot on device)  
+#' *width*: the width of the device/file (default: 20cm)  
+#' *height*: the height of the device/file (default: 15cm)  
+#' *units*: The units in which height and width are given (default: 'cm')
+#'          Could be 'in', 'px', 'mm'  
+#' *res*: The nominal resolution in ppi which will be recorded in the bitmap file (default: 300)  
+#' **Return:**  
+#' none  
+#' **Examples:** `plotToDeviceOrFile(data, filepath='fig1.png')`  
+plotToDeviceOrFile<-function(data, col=NULL,
+                             main_title=NULL, xlabel=NULL, ylabel=NULL,
+                             filepath=NULL, width=20, height=15, units='cm',
+                             res=300)
+{
+  if (FALSE == is.null(filepath)) {
+    png(filename=filepath, witdth=width, height=height, units=units, res=res)
+  }
+  plot(data, col=col, main=main_title, xlab=xlabel)
+  if (FALSE == is.null(filepath)) {
+    dev.off()
+  }
 }
